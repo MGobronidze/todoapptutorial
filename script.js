805028a08d97b5dddc1c209bcb25240a371e5855
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2024-04-05 23:29:55 +0400

    add todo app

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("Please fill the input field")
+    }else{
+        let liEl = document.createElement("li");
+        liEl.innerHTML = inputBox.value;
+        listContainer.appendChild(liEl);
+        let spanEl =document.createElement("span");
+        spanEl.innerHTML = "\u00d7";
+        liEl.appendChild(spanEl)
+    }
+    inputBox.value=""
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
